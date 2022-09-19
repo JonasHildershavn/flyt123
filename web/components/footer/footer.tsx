56b372c1fa80153fbb6d46ff12000796b7c5c6fc
Ask Strande <ask.strande@knowit.no>
2022-09-19 09:02:35 +0200

    Setup of simple structure and styling

---
+++ web/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+const Footer: FC = () => (
+    <footer className="footer">
+        Copyright Knowit
+    </footer>
+)
+
+export default Footer
---
+++ web/components/header/header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header: React.FC = () => (
+  <header className="header">
+    <h1>Flyt</h1>
+    <span className="header__tagline">Plattformen for deg som flyter</span>
+  </header>
+);
+
+export default Header;
