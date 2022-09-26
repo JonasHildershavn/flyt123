bae0865fb2aac0e8d79b239349a4aaa11cbc8593
Christian Olsen <christian.olsen@knowit.no>
2022-09-26 09:21:58 +0200

    project overview setup

--- web/components/project-overview/project-overview.tsx
+++ web/components/project-overview/project-overview.tsx
@@ -1,6 +1,7 @@
 const ProjectOverview: React.FC = () => (
     <main className="project">
         Prosjekter for alle sammen!
+        {/* TODO: cardview av prosjekter */}
     </main>
 )
 
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,14 +1,38 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import groq from 'groq'
+import client from '../client'
 
 import PageLayout from '../components/page-layout/page-layout';
 import ProjectOverview from '../components/project-overview/project-overview';
 
-const Home: NextPage = () => {
+const Home: NextPage = ({projects}) => {
   return (
     <PageLayout>
-      <ProjectOverview />
+      <ProjectOverview/>
+      {projects.length > 0 && projects.map(
+        ({ _id, title = '', slug = ''}) =>
+          slug && (
+            <li key={_id}>
+              <Link href="/post/[slug]" as={`/post/${slug.current}`}>
+                <a>{title}</a>
+              </Link>{' '}
+              
+            </li>
+          )
+      )}
     </PageLayout>
   )
+}
+
+export async function getStaticProps() {
+  const projects = await client.fetch(groq`
+      *[_type == "post"]`)
+  return {
+    props: {
+      projects
+    }
+  }
 }
 
 export default Home
