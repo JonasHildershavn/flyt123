837afde8ba9af305fc78835c3913a03a8a7315fb
Ask Strande <ask.strande@knowit.no>
2022-09-29 10:52:52 +0200

    try to resolve conflict

---
+++ web/pages/index.js
@@ -0,0 +1,27 @@
+import { NextPage } from 'next'
+import groq from 'groq'
+import client from '../client'
+
+import PageLayout from '../components/page-layout/page-layout';
+import ProjectOverview from '../components/project-overview/project-overview';
+
+const Home = ({ projects }) => {
+  return (
+    <PageLayout>
+      <ProjectOverview projects={projects}/>
+    </PageLayout>
+  )
+}
+
+export async function getStaticProps() {
+  const projects = await client.fetch(groq`
+      *[_type == "project"]|order(publishedAt desc)
+    `)
+  return {
+    props: {
+      projects
+    }
+  }
+}
+
+export default Home
