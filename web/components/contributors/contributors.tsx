f4d83afdaabd027be5b374bc97a97eebfe05ed39
Ask Strande <ask.strande@knowit.no>
2022-10-06 16:06:04 +0200

    fix of build fail

--- web/components/contributors/contributors.tsx
+++ web/components/contributors/contributors.tsx
@@ -1,16 +1,22 @@
-import Heading from '../heading/heading';
+import Heading from "../heading/heading";
+
+import { SanityAuthor } from "../../models/sanity-author";
 
 interface ContributionsProps {
-  contributors: string[];
+  contributors: SanityAuthor[];
 }
 
 const Contributors: React.FC<ContributionsProps> = ({ contributors }) => (
-  <div className='contributors'>
-    <Heading headingLevel='h2' className='contributors__heading'>Tusen takk for alle bidrag - store og små</Heading>
+  <div className="contributors">
+    <Heading headingLevel="h2" className="contributors__heading">
+      Tusen takk for alle bidrag - store og små
+    </Heading>
     {contributors && contributors.length > 0 && (
-      <ul className='contributors__list'>
+      <ul className="contributors__list">
         {contributors.map((element, index) => (
-            <li className='contributors__list-item' key={index}>{element.name}</li> //TODO: fix redline
+          <li className="contributors__list-item" key={index}>
+            {element.name}
+          </li> //TODO: fix redline
         ))}
       </ul>
     )}
