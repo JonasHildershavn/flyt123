29364906b6d0bcb9e83d4d6f6199fb4adc796f4f
Ask Strande <ask.strande@knowit.no>
2022-10-07 14:28:48 +0200

    fix for footer and pagelayout

--- web/components/page-layout/page-layout.tsx
+++ web/components/page-layout/page-layout.tsx
@@ -1,16 +1,19 @@
-import Header from '../header/header'
-import Footer from '../footer/footer'
+import Header from "../header/header";
+import Footer from "../footer/footer";
+import Container from "../container/container";
 
 interface PageLayoutProps {
-    children?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
-const PageLayout: React.FC<PageLayoutProps> = ({children}) => (
-    <div className="page-layout">
-        <Header />
-        {children}
-        <Footer />
-    </div>
-)
+const PageLayout: React.FC<PageLayoutProps> = ({ children }) => (
+  <div className="page-layout">
+    <Header />
+    <Container className="page-layout__content" theme="wide">
+      {children}
+    </Container>
+    <Footer />
+  </div>
+);
 
-export default PageLayout
+export default PageLayout;
