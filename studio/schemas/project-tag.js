b621c082093efd13de3fb9659c2cacc3f753ab69
andersgsteen <anders.g.steen@knowit.no>
2022-10-28 12:58:23 +0200

    Added category to project tags

--- studio/schemas/project-tag.js
+++ studio/schemas/project-tag.js
@@ -7,6 +7,21 @@ export default {
         name: 'tag',
         title: 'Tag',
         type: 'string',
+      },
+      {
+        name: 'category',
+        title: 'Kategori',
+        type: 'string',
+        options: {
+          list: [
+            {title: 'Utvikling', value: 'development'},
+            {title: 'Design', value: 'design'},
+            {title: 'Innhold', value: 'content'},
+            {title: 'Administrering/Ledelse', value: 'administration'},
+            {title: 'Annet', value: 'other'},
+          ],
+          layout: 'radio'
+        }
       }
     ]
   };
