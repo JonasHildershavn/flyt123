3502ebf2db9255d257b086e9ee0ea08b30b0e2d7
andersgsteen <anders.g.steen@knowit.no>
2022-11-04 16:17:41 +0100

    Added mouse follow animation to logo

--- web/components/logo/logo.tsx
+++ web/components/logo/logo.tsx
@@ -3,19 +3,20 @@ import { useEffect, useRef, useState } from "react";
 
 const Logo: React.FC = () => {
     const floatingDiv = useRef<null | HTMLDivElement>(null)
-    //let [mouseX, setMouseX] = useState(0);
     let mouseX = 0
-    const letterL = useRef<null | HTMLSpanElement>(null)
-    //const [width, setWidth] = useState(0)
+    let mouseXL = 0
+    let mouseXY = 0
     let width = 0 
+    let endX = 0
     const previousTimeRef = useRef<null | number>(null)
-    const letterY = useRef<null | HTMLSpanElement>(null)
+    const letterY = useRef<null | HTMLDivElement>(null)
+    const letterL = useRef<null | HTMLDivElement>(null)
     const requestRef = useRef<number>(0)
     useEffect(() => {
         if (floatingDiv.current != null) {
             floatingDiv.current.onmousemove = handleMouseMove;
-            floatingDiv.current.onmouseleave = () => handleMouseLeave;
-            //setWidth(floatingDiv.current.getBoundingClientRect().width)
+            floatingDiv.current.onmouseleave = handleMouseLeave;
+            floatingDiv.current.onmouseenter = handleMouseEnter
             width = floatingDiv.current.getBoundingClientRect().width
         }
         requestRef.current = requestAnimationFrame(animateLetters);
@@ -28,25 +29,38 @@ const Logo: React.FC = () => {
 
     const handleMouseLeave = (ev: any) => {
         cancelAnimationFrame(requestRef.current);
+        endX = 0
+    }
+
+    const handleMouseEnter = (ev: any) => {
+        requestRef.current = requestAnimationFrame(animateLetters)
     }
 
     const onResize = () => {
         if (floatingDiv.current != null) {
-            //setWidth(floatingDiv.current.getBoundingClientRect().width)
             width = floatingDiv.current.getBoundingClientRect().width
         }
     }
-
-    let endX = width / 2;
 
     const animateLetters = (time: number) => {
         if (previousTimeRef.current != null
             && letterL.current != null
             && letterY.current != null) {
-          mouseX += (endX - mouseX) / 15;
-          console.log(mouseX)
-          letterL.current.style.paddingLeft = mouseX + "px";
-          letterY.current.style.paddingLeft = mouseX + "px";
+
+          mouseXL += endX < mouseXL ? (endX - mouseXL) / 7 : (endX - mouseXL) / 30
+          mouseXY += endX < mouseXY ? (endX - mouseXY) / 30 : (endX - mouseXY) / 7
+          //mouseX += (endX - mouseX) / 15;
+          const widthL = width - letterL.current.getBoundingClientRect().width
+          const widthY = width - letterL.current.getBoundingClientRect().width
+          //const leftL = (mouseX / width) * (widthL / width)
+          //const leftY = (mouseX / width) * (widthY / width)
+
+
+          const leftL = mouseXL * widthL
+          const leftY = mouseXY * widthY
+          
+          letterL.current.style.transform = `translateX(${leftL}px)`;
+          letterY.current.style.transform = `translateX(${leftY}px)`;
         }
         previousTimeRef.current = time;
         requestRef.current = requestAnimationFrame(animateLetters);
@@ -61,7 +75,8 @@ const Logo: React.FC = () => {
             const relativeLeft = x - containerRect.left;
             // setLeft(relativeLeft * 100);
             // console.log("left:", relativeLeft + "%")
-            endX = relativeLeft
+
+            endX = relativeLeft / width//relativeLeft
             //setMouseX(relativeLeft)
             //mouseX = relativeLeft
         }
@@ -71,8 +86,8 @@ const Logo: React.FC = () => {
             <a className="logo">
                 <span className="f">F</span>
                 <div ref={floatingDiv} className="logo__floating-container">
-                    <span ref={letterL} className="l">L</span>
-                    <span ref={letterY} className="y">Y</span>
+                    <div ref={letterL} className="l">L</div>
+                    <div ref={letterY} className="y">Y</div>
                 </div>
                 <span className="t">T</span>
             </a>
