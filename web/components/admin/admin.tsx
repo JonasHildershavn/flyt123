815acbcd1399f8a021a84939e7fe635e88454b7f
Elise Nordal <elise.nordal@knowit.no>
2022-11-04 09:13:59 +0100

    fix adminpage

--- web/components/admin/admin.tsx
+++ web/components/admin/admin.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 
 const Admin: React.FC = () => (
-    <admin className="admin">
+    <div className="admin">
         <h2>Adminpanel</h2>
         <p>Her ser du oversikt over tilgjengelige ressurser. Ta kontakt med dem på Slack eller epost for å avtale nærmere, og minn dem på å oppdatere kapasiteten sin!</p>
-    </admin>
+    </div>
 
 )
 
