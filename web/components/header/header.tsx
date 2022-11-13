4bb033a0da0dd8d19876f9ef9927088d1935d00f
Ask Strande <ask.strande@knowit.no>
2022-11-13 17:55:42 +0100

    anim

--- web/components/header/header.tsx
+++ web/components/header/header.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
+import cn from "classnames";
+
 import Container from "../container/container";
 import LinkButton from "../link-button/link-button";
 
 const Header: React.FC = () => {
   const [numLikes, setNumLikes] = useState(0);
+  const [anim, setAnim] = useState(false);
 
   useEffect(() => {
     const currentItems: Array<string> =
@@ -18,6 +21,9 @@ const Header: React.FC = () => {
           ? JSON.parse(String(localStorage.getItem("likes")))
           : [];
       setNumLikes(currentItems.length);
+
+      setAnim(true);
+      setTimeout(() => setAnim(false), 500);
     });
   }, []);
 
@@ -34,7 +40,15 @@ const Header: React.FC = () => {
             text="Meld inn ledig tid"
             theme="transparent"
           />
-          <span className="header__counter">{numLikes}</span>
+          <span
+            key="counter"
+            className={cn(
+              "header__counter",
+              anim ? "header__counter--pulse" : null
+            )}
+          >
+            {numLikes}
+          </span>
         </div>
       </Container>
     </header>
