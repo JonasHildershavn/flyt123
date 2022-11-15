3dbe7f6feed756adfe99ad6c34ef333f12d27b38
Elise Nordal <elise.nordal@knowit.no>
2022-11-15 14:45:20 +0100

    fix styling in header

--- web/components/header/header.tsx
+++ web/components/header/header.tsx
@@ -1,8 +1,21 @@
+import Link from 'next/link';
 import Logo from '../logo/logo';
 
 const Header: React.FC = () => (
   <header className="header">
-    <Logo/>
+    <span className='header__logo'>
+      FLYT 
+    </span>
+    <div className ="header__right-container">
+      <span className ="header__text">
+       <Link href={"ledig-tid/1"}>
+          <a> Mine interesser</a>  
+       </Link>
+      </span>
+      <div className = "header__indicator">
+              1
+       </div>
+    </div>
   </header>
 );
 
