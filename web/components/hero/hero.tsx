b8a9b54d29f8413d7803bcfb0803397d58ab4e9d
Elise Nordal <elise.nordal@knowit.no>
2022-11-15 14:45:53 +0100

    fix hero

--- web/components/hero/hero.tsx
+++ web/components/hero/hero.tsx
@@ -4,15 +4,6 @@ import Container from "../container/container";
 const Hero: React.FC = () => (
   <section className="hero">
     <Container className="task-overview__container" theme="wide">
-      <Link href="ledig-tid/1">
-        <a className="hero__ledig-tid-btn">
-          <span>MELD INN LEDIG TID</span>
-        </a>
-      </Link>
-      <h2 className="hero__text">
-        Velkommen til Flyt! Her finner du oversikt over internprosjekter og
-        oppgaver man kan bidra med når man har ledig tid
-      </h2>
     </Container>
   </section>
 );
