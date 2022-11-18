1b6a6f67f174796f8975663f81d44f58b723e611
JonasHildershavn <jonas.hildershavn@gmail.com>
2022-11-18 10:35:40 +0100

    Comment out admin.tsx stuff

--- web/pages/admin.tsx
+++ web/pages/admin.tsx
@@ -1,37 +1,36 @@
-import { NextPage } from 'next'
-import PageLayout from '../components/page-layout/page-layout';
-import VacantTable from '../components//vacant-table/vacant-table';
+import { NextPage } from "next";
+import PageLayout from "../components/page-layout/page-layout";
+import VacantTable from "../components//vacant-table/vacant-table";
 
-import { AzureVacant } from '../models/azure-vacant';
-import { Storage } from '../api/azure-storage';
+import { AzureVacant } from "../models/azure-vacant";
+import { Storage } from "../api/azure-storage";
 
 interface Props {
-    vacants: AzureVacant[]
+  //   vacants: AzureVacant[];
 }
 
-
-const AdminPage : NextPage<Props> = ({
-    vacants,
-}) => {
-    
-    return (
-        <PageLayout title="Adminpanel">
-            <h2>Admin</h2>
-            <VacantTable array={vacants}/>
-        </PageLayout>
-    )
-}
+const AdminPage: NextPage<Props> = (
+  {
+    // vacants
+  }
+) => {
+  return (
+    <PageLayout title="Adminpanel">
+      <h2>Admin</h2>
+      {/* <VacantTable array={vacants} /> */}
+    </PageLayout>
+  );
+};
 
 export async function getStaticProps() {
-    let storage = new Storage();
-    let vacants = await storage.getVacants();
+  //   let storage = new Storage();
+  // let vacants = await storage.getVacants();
 
-    return {
-        props: {
-            vacants,
-        }
-    }
+  return {
+    props: {
+      //   vacants,
+    },
+  };
 }
 
-export default AdminPage
-
+export default AdminPage;
