1a3e51a98c49b1c7123e42a5bdf5b9a90cc97243
JonasHildershavn <jonas.hildershavn@gmail.com>
2022-11-25 09:24:35 +0100

    Add next-auth to project

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,16 +1,18 @@
 import "../styles/site.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: any) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Head>
         <link rel="shortcut icon" href="/flyt_logo.ico" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
-
-export default MyApp;
